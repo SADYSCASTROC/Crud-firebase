87905bd4eabba11c6dd4bc4b939c38efc49140fc
unknown <sadyscastro@gmil.com>
2023-01-09 22:59:57 -0600

    add function edit

--- src/components/LinkForms.js
+++ src/components/LinkForms.js
@@ -1,31 +1,45 @@
 // Permitir añadir un nuevo enlace
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
+import { db } from "../firebase";
 
 const LinkForm = (props) => {
-
-    const inicialStatateValues ={
-        url:'',
-        name:'',
-        description:''
+    const inicialStatateValues = {
+        url: '',
+        name: '',
+        description: ''
     };
 
     const [values, setValues] = useState(inicialStatateValues)
 
     //Funcion para onchange Llame a una función cuando un usuario cambie el contenido de un campo de entrada
-    const handleInputtChange = (e) =>{
-        const {name, value} = e.target
-        setValues({...values,[name]:value})
+    const handleInputtChange = (e) => {
+        const { name, value } = e.target
+        setValues({ ...values, [name]: value })
     }
 
-//Crear una función para el envio de la informacion evento del boton guardar
-// onsubmit Llame a una función cuando se envía un formulario
-const handeleSubmi = (e) => {
-    e.preventDefault();
-    props.addOrEditLink(values);
-    setValues({...inicialStatateValues})
-}
+    //Crear una función para el envio de la informacion evento del boton guardar
+    // onsubmit Llame a una función cuando se envía un formulario
+    const handeleSubmi = (e) => {
+        e.preventDefault();
+        props.addOrEditLink(values);
+        setValues({ ...inicialStatateValues })
+    }
+
+    const getLinksById = async (id) => {
+     const doc =  await db.collection('links').doc(id).get();
+     console.log(doc.data())
+    }
+
+    useEffect(() => {
+        if (props.currenId === '') {
+            setValues({ ...inicialStatateValues })
+        } else {
+            getLinksById(props.currenId);
+        }
+
+    }, [props.currenId]);
 
     return (
         <form className="card card-body" onSubmit={handeleSubmi}>
@@ -33,14 +47,13 @@ const handeleSubmi = (e) => {
                 <div className="input-group-text bg-light">
                     <i className="material-icons">insert_link</i>
                 </div>
-                <input type="text" className="form-control" placeholder="https://someurl.com" name="url"  onChange={handleInputtChange } value={values.url}/>
+                <input type="text" className="form-control" placeholder="https://someurl.com" name="url" onChange={handleInputtChange} value={values.url} />
             </div>
 
             <div className="form-group input-group p-2">
                 <div className="input-group-text bg-light">
-                    <i className="material-icons">create</i>
                 </div>
-                <input type="text" className="form-control" name="name" placeholder="Nombre del sitio Web" onChange={handleInputtChange} value={values.name}/>
+                <input type="text" className="form-control" name="name" placeholder="Nombre del sitio Web" onChange={handleInputtChange} value={values.name} />
             </div>
 
             <div className="form-group p-2">
--- src/components/Links.js
+++ src/components/Links.js
@@ -2,18 +2,20 @@
 import React, { useEffect, useState } from "react";
 import LinkForm from "./LinkForms";
 import swal from "sweetalert";
-import {toast, Toast}  from "react-toastify";
-
-
+import { toast, Toast } from "react-toastify";
 import { db } from '../firebase'
-import { type } from "@testing-library/user-event/dist/type";
+
+
 const Links = () => {
 
     const [links, setLinks] = useState([])
+    const [currenId, setcurrenId] = useState('')
+
+
 
     const addOrEditLink = async (linkObject) => {
         await db.collection('links').doc().set(linkObject);
-        toast('Nueva publicacion agregada',{
+        toast('Nueva publicacion agregada', {
             type: 'success'
         })
     }
@@ -23,11 +25,11 @@ const Links = () => {
             title: "Esta seguro que desea eliminar el archivo",
             buttons: ["Cancelar", "Elininar"]
         })).then(respuesta => {
-            if(respuesta){
-             db.collection('links').doc(id).delete();
+            if (respuesta) {
+                db.collection('links').doc(id).delete();
                 console.log('eliminada')
                 swal({
-                    text:"Se ha borrado con exito",
+                    text: "Se ha borrado con exito",
                     icon: "success"
                 })
             }
@@ -52,7 +54,7 @@ const Links = () => {
     return (
         <div>
             <div className="col-md-4 p-2">
-                <LinkForm addOrEditLink={addOrEditLink} />
+                <LinkForm {...{addOrEditLink, currenId, links}} />
                 <div className="col-md-8 p-4 w-100" >
                     {links.map(link => (
                         <div className="card mb-1 p-2 text-center" key={link.id}>
@@ -63,6 +65,7 @@ const Links = () => {
                             </div>
                             <div>
                                 <i className="material-icons text-danger" onClick={() => onDeliteLink(link.id)}>delete</i>
+                                <i className="material-icons" onClick={()=>setcurrenId(link.id)}>create</i>
                             </div>
                         </div>
                     ))}
